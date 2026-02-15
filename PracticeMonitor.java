c7551a4748dc885f6ccea5651e96d2b78ddadabe
David Nguyen <davidnguyenssis@gmail.com>
2026-02-15 12:51:16 -0600

    fixed practicemonitor

--- PracticeMonitor.java
+++ PracticeMonitor.java
@@ -1,9 +1,28 @@
-public class practiceMontitor(){
-    private int practicesPerWeek;
-    public void setPractices(int i){
-        practicesPerWeek=i;
-    }
-    public void getPracticesPerWeek(){
+public class PracticeMonitor extends AthleteList{
+      private int praticesPerWeek;
+    
+      public PracticeMonitor (String sport, String team, int practicesPerWeek)
+      {
+        super(sport, team);
+        this.practicesPerWeek = 5;
+      }
+
+      public boolean setPracticesPerWeek (Coach coach,int practices)
+      {
+        if ((!coach.authentic())||((practices<0)||(practices>5)))
+        {
+          return false;
+        }
+        else
+        {
+          this.practicesPerWeek = practices;
+          return true;
+        }
+      }
+
+      public int getPracticesPerWeek()
+      {
         return practicesPerWeek;
-    }
+      }
+
 }
